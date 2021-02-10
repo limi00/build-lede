bb92bbfaf345f5b1f9912e8642447ac01facc3d7
limi00 <75186809+limi00@users.noreply.github.com>
2021-02-10 17:37:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git project https://github.com/project-openwrt/openwrt' feeds.conf.default
+sed -i '$a src-git openwrt https://github.com/openwrt/packages' feeds.conf.default
 
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
