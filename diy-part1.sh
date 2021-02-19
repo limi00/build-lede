dc792f2b8622105846b27a3de3e915e9a1be2651
limi00 <75186809+limi00@users.noreply.github.com>
2021-02-20 07:43:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,9 @@
 sed -i '$a src-git project https://github.com/project-openwrt/openwrt' feeds.conf.default
 sed -i '$a src-git openwrt https://github.com/openwrt/packages' feeds.conf.default
 
+# # Define My Package
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/diy/luci-app-adguardhome
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw/luci-app-usb3disable package/diy/luci-app-usb3disable
+git clone https://github.com/immortalwrt/openwrt-passwall.git package/diy/luci-app-passwall
+
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
