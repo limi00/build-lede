d83d89543513971363ced5f588d844f0ff26ef71
limi00 <75186809+limi00@users.noreply.github.com>
2021-02-26 13:29:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,10 +20,11 @@
 
 # # Define My Package
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/diy/luci-app-adguardhome
+# git clone https://github.com/immortalwrt/openwrt-passwall.git package/diy/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/diy/luci-app-passwall
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw/luci-app-usb3disable package/diy/luci-app-usb3disable
-git clone https://github.com/immortalwrt/openwrt-passwall.git package/diy/luci-app-passwall
+svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-autotimeset package/diy/luci-app-autotimeset
 # svn co https://github.com/linkease/ddnsto-openwrt/trunk/ddnsto package/network/services/ddnsto
 # svn co https://github.com/linkease/ddnsto-openwrt/trunk/luci-app-ddnsto package/diy/luci-app-ddnsto
-svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-autotimeset package/diy/luci-app-autotimeset
 
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
