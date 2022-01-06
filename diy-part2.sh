d26a456d6f3b908a7b7c11baa4fae74cd0adbdc0
limi00 <75186809+limi00@users.noreply.github.com>
2022-01-07 06:53:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/xiaomi_mir3g/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Mi R3G/g' package/base-files/files/bin/config_generate
 # sed -i '/uci commit system/i\uci set system.@system[0].hostname='XiaomiR3G'' package/default-settings/files/zzz-default-settings
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
