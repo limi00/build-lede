4d284f5a5f3da52531599f2a36ee74de01677c54
limi00 <75186809+limi00@users.noreply.github.com>
2022-05-22 19:08:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -31,11 +31,11 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
 
 # # Define My Package
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/diy/luci-app-adguardhome
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/diy/luci-app-passwall2
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/diy/luci-app-adguardhome
+# git clone https://github.com/xiaorouji/openwrt-passwall2.git package/diy/luci-app-passwall2
 # git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 # git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
-git clone https://github.com/rufengsuixing/luci-app-usb3disable.git package/diy/luci-app-usb3disable
+# git clone https://github.com/rufengsuixing/luci-app-usb3disable.git package/diy/luci-app-usb3disable
 # git clone https://github.com/sirpdboy/netspeedtest.git package/diy/luci-app-netspeedtest
 # git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/diy/luci-app-autotimeset
 # git clone https://github.com/zzsj0928/luci-app-pushbot package/diy/luci-app-pushbot
