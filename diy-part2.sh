5e183a6e59f3c16be3a35a7ee608bd6bc8eaede8
limi00 <75186809+limi00@users.noreply.github.com>
2022-08-08 11:31:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,5 +17,8 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 sed -i 's/OpenWrt/XiaoMi_R3G/g' package/base-files/files/bin/config_generate
 # sed -i '/uci commit system/i\uci set system.@system[0].hostname='XiaomiR3G'' package/default-settings/files/zzz-default-settings
 
+# 修改默认 SSID
+sed -i 's/OpenWrt/Xiaomi_DC08/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
